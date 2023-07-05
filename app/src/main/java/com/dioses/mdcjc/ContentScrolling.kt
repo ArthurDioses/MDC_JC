394db4be2ba49523b90a25dd008069eecc812235
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-07-05 00:55:20 -0500

    - Using Scaffold

--- app/src/main/java/com/dioses/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/dioses/mdcjc/ContentScrolling.kt
@@ -2,11 +2,13 @@ package com.dioses.mdcjc
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dioses.mdcjc.ui.theme.MDCJCTheme
 
 
@@ -19,10 +21,12 @@ fun ContentPreview() {
 }
 
 @Composable
-fun Content() {
+fun Content(modifier: Modifier = Modifier) {
     Text(
         text = "Hola !", modifier = Modifier
             .fillMaxSize()
+            .padding(16.dp)
             .background(Color.Cyan)
+
     )
 }
