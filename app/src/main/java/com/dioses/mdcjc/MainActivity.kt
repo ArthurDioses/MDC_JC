36bad4254047d1e09fa9d8451844cec66a954ed8
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-07-05 15:19:11 -0500

    - BottomAppBar

--- app/src/main/java/com/dioses/mdcjc/MainActivity.kt
+++ app/src/main/java/com/dioses/mdcjc/MainActivity.kt
@@ -3,12 +3,21 @@ package com.dioses.mdcjc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.dioses.mdcjc.ui.theme.MDCJCTheme
@@ -30,7 +39,20 @@ class MainActivity : ComponentActivity() {
 fun ContentMain() {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        containerColor = MaterialTheme.colorScheme.background
+        containerColor = MaterialTheme.colorScheme.background,
+        bottomBar = {
+            BottomAppBar {
+                CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onSurface) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                    }
+                }
+                Spacer(modifier = Modifier.weight(1f, true))
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.MoreVert, contentDescription = "Options")
+                }
+            }
+        }
     ) { padding ->
         Content(Modifier.padding(padding))
     }
