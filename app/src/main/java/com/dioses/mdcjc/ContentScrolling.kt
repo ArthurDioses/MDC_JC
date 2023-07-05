4d9de958d26a4952769278b76539353bf06c8ad7
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-07-05 18:51:16 -0500

    - Columns

--- app/src/main/java/com/dioses/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/dioses/mdcjc/ContentScrolling.kt
@@ -1,12 +1,15 @@
 package com.dioses.mdcjc
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dioses.mdcjc.ui.theme.MDCJCTheme
 
 
@@ -24,8 +27,13 @@ fun Content(modifier: Modifier = Modifier) {
         modifier = modifier
             .verticalScroll(rememberScrollState())
     ) {
-        Text(text = "Cursos")
-        Text(text = "Android")
-        Text(text = "ANT")
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Column {
+            }
+        }
     }
 }
