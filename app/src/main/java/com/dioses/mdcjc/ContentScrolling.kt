068e8642b9cbe2cda52ec77152438b456393d6e8
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-07-06 00:00:02 -0500

    - Add OutlinedTextField for image url input - Add TextField for message required.

--- app/src/main/java/com/dioses/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/dioses/mdcjc/ContentScrolling.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.dioses.mdcjc
 
 import androidx.compose.foundation.Image
@@ -9,7 +11,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -33,8 +37,7 @@ fun ContentPreview() {
 @Composable
 fun Content(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier
-            .verticalScroll(rememberScrollState())
+        modifier = modifier.verticalScroll(rememberScrollState())
     ) {
         Card(
             modifier = Modifier
@@ -58,6 +61,30 @@ fun Content(modifier: Modifier = Modifier) {
                         .fillMaxWidth()
                         .padding(dimensionResource(id = R.dimen.common_padding_default)),
                     style = MaterialTheme.typography.headlineMedium
+                )
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = { Text(text = stringResource(id = R.string.card_input_url)) },
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            top = dimensionResource(id = R.dimen.common_padding_default),
+                            start = dimensionResource(id = R.dimen.common_padding_default),
+                            end = dimensionResource(id = R.dimen.common_padding_default)
+                        )
+                )
+                Text(
+                    text = stringResource(id = R.string.card_required),
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8f),
+                    modifier = Modifier.padding(
+                        start = dimensionResource(id = R.dimen.card_padding_default),
+                        top = dimensionResource(
+                            id = R.dimen.card_padding_micro
+                        )
+                    )
                 )
             }
         }
