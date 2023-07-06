176faff1e64eb881af3ec6c8de095a0f71d7f5e9
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-07-05 22:01:15 -0500

    - Add Text with style headlines medium.

--- app/src/main/java/com/dioses/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/dioses/mdcjc/ContentScrolling.kt
@@ -9,11 +9,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dioses.mdcjc.ui.theme.MDCJCTheme
@@ -46,6 +49,15 @@ fun Content(modifier: Modifier = Modifier) {
                         .fillMaxWidth()
                         .height(dimensionResource(id = R.dimen.card_img_cover_height))
                         .background(colorResource(id = R.color.teal_200))
+                )
+                Text(
+                    text = stringResource(
+                        id = R.string.card_title
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(dimensionResource(id = R.dimen.common_padding_default)),
+                    style = MaterialTheme.typography.headlineMedium
                 )
             }
         }
