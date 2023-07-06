d8b243ba4ae65f496ebe738519f0c96e07f480c0
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-07-05 19:01:35 -0500

    - Add dimens file - Add vector ic_shop - Add Image in Compose

--- app/src/main/java/com/dioses/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/dioses/mdcjc/ContentScrolling.kt
@@ -1,13 +1,19 @@
 package com.dioses.mdcjc
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dioses.mdcjc.ui.theme.MDCJCTheme
@@ -33,6 +39,14 @@ fun Content(modifier: Modifier = Modifier) {
                 .padding(8.dp)
         ) {
             Column {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_shop),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(dimensionResource(id = R.dimen.card_img_cover_height))
+                        .background(colorResource(id = R.color.teal_200))
+                )
             }
         }
     }
