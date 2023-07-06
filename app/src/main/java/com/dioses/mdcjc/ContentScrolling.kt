50cb9efc643f59985f6a5f1c73acf606e85a4fb5
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-07-06 00:37:11 -0500

    - Add CheckBox for enable Input Password

--- app/src/main/java/com/dioses/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/dioses/mdcjc/ContentScrolling.kt
@@ -2,12 +2,14 @@ package com.dioses.mdcjc
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -17,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
@@ -111,6 +114,12 @@ fun Content(modifier: Modifier = Modifier) {
                             end = dimensionResource(id = R.dimen.common_padding_default)
                         )
                 )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(
+                        checked = isCheckboxChecked,
+                        onCheckedChange = { isCheckboxChecked = it })
+                    Text(text = stringResource(id = R.string.card_enable_pass))
+                }
             }
         }
     }
